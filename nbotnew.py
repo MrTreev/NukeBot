28e2e2d26b9a2cd98ccd2685d81927b2980c2580
MrTreev <mr.treev@gmail.com>
2020-12-15 15:09:41 +1100

    created gib command for tags and merged it with gibcode

--- nbotnew.py
+++ nbotnew.py
@@ -1,13 +1,15 @@
+import os
 import discord, nhentai, random, requests
 from discord.ext import commands
+fileDir = os.path.dirname(os.path.abspath(__file__))
 keys={}
-nuke=open("nuke.token", "r")
+nuke=open(fileDir+"/nuke.token", "r")
 for line in nuke:
-    (key,val)=line.split('>')
+    (key,val)=line.split(' ')
     keys[key]=val
 
 
-bot = commands.Bot(command_prefix='!')
+bot = commands.Bot(command_prefix='>')
 
 @bot.command()
 async def ping(ctx):
@@ -15,6 +17,7 @@ async def ping(ctx):
 
 @bot.command()
 async def nukemedaddy(ctx):
+    await ctx.send("Hacking the NNSA...")
     valid = False
     while valid == False:
         code = ''
@@ -34,13 +37,45 @@ async def nukemedaddy(ctx):
     await ctx.send(name+"\n"+thumb+"\n"+code+"\n"+str(res))
 
 @bot.command()
-async def gibcode(ctx, arg):
-    doujin=nhentai.get_doujin(int(arg))
-    thumb=doujin.thumbnail
-    name=doujin.titles['english']
-    res = []
-    for i in doujin.tags:
-        if i[0] not in res and (i != ''):
-            res.append(i[2])
-    await ctx.send(name+"\n"+thumb+"\n"+str(res))
+async def gib(ctx, *, args):
+    if args.isdigit():
+        try:
+            await ctx.send("processing code...")
+            doujin=nhentai.get_doujin(int(args))
+            thumb=doujin.thumbnail
+            name=doujin.titles['english']
+            res = []
+            for i in doujin.tags:
+                if i[0] not in res and (i != ''):
+                    res.append(i[2])
+            await ctx.send(name+"\n"+thumb+"\n"+str(res))
+        except:
+            await ctx.send("unable to fulfil request")
+    elif isinstance(args,str):
+        try:
+            await ctx.send("processing tag...")
+            o=1
+            ret=[]
+            tag=1
+            while tag==1:
+                listo=nhentai.search(args,o)
+                if listo==[]:
+                    tag=0
+                for i in listo:
+                    ret.append(str(i.id))
+                o=o+1
+            key=random.randint(0,len(ret)-1)
+            doujin=nhentai.get_doujin(int(ret[key]))
+            name=doujin.titles['english']
+            thumb=doujin.thumbnail
+            res = []
+            for i in doujin.tags:
+                if i[0] not in res and (i != ''):
+                    res.append(i[2])
+            await ctx.send(name+"\n"+thumb+"\n"+ret[key]+"\n"+str(res))
+        except:
+            await ctx.send("unable to fulfil request")
+    else:
+        await ctx.send("WTF are you asking for?")
+
 bot.run(keys['TOKEN'])
