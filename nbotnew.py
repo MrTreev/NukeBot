b00b56b6bcf03778256d4dd3cb24f74ae9cdf767
MrTreev <mr.treev@gmail.com>
2020-12-21 16:49:57 +1100

    added cursed tag functionality

--- nbotnew.py
+++ nbotnew.py
@@ -2,18 +2,24 @@ import os
 import discord, nhentai, random, requests
 from discord.ext import commands
 fileDir = os.path.dirname(os.path.abspath(__file__))
+cursedfile=open(fileDir+"/cursed.tags","r")
+cursed=cursedfile.read().splitlines()
+cursedfile.close()
+bot = commands.Bot(command_prefix='>')
 keys={}
 nuke=open(fileDir+"/nuke.token", "r")
 for line in nuke:
     (key,val)=line.split(' ')
     keys[key]=val
+nuke.close()
 
 
-bot = commands.Bot(command_prefix='>')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
 
 @bot.command()
 async def nukemedaddy(ctx):
@@ -34,7 +40,30 @@ async def nukemedaddy(ctx):
     for i in doujin.tags:
         if i[0] not in res and (i != ''):
             res.append(i[2])
-    await ctx.send(name+"\n"+thumb+"\n"+code+"\n"+str(res))
+    user=ctx.message.author.id
+    server=ctx.guild.id
+    add=0
+    for tag in res:
+        if tag in cursed:
+            add=add+1
+    print("add="+str(add))
+    try:
+        userCursed=open(fileDir+"/servers"+"/"+str(server)+"/"+str(user),"r")
+        userCursedVal=int(userCursed.readline())
+        userCursed.close()
+        userCursed=open(fileDir+"/servers"+"/"+str(server)+"/"+str(user),"w")
+    except:
+        os.mkdir(fileDir+"/servers"+"/"+str(server))
+        userCursed=open(fileDir+"/servers"+"/"+str(server)+"/"+str(user),"w")
+        userCursed.write("0")
+        userCursedVal=0
+    cursedMessage="Cursed Value: "+str(add)
+    await ctx.send(name+"\n"+thumb+"\n"+code+"\n"+str(res)+"\n"+cursedMessage)
+    newCursedVal=str(userCursedVal+add)
+    userCursed.write(newCursedVal)
+    userCursed.close()
+
+
 
 @bot.command()
 async def gib(ctx, *, args):
@@ -77,5 +106,21 @@ async def gib(ctx, *, args):
             await ctx.send("unable to fulfil request")
     else:
         await ctx.send("WTF are you asking for?")
+
+
+
+@bot.command()
+async def checkcurse(ctx):
+    user=ctx.message.author.id
+    server=ctx.guild.id
+    try:
+        userCursed=open(fileDir+"/servers"+"/"+str(server)+"/"+str(user),"r")
+        userCursedVal=int(userCursed.readline())
+        userCursed.close()
+        await ctx.send("Your cursed value is: "+str(userCursedVal))
+    except:
+        await ctx.send("You have no cursed value yet")
+
+
 
 bot.run(keys['TOKEN'])
